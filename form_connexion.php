5d5df6d829ae2414e1f91387c88d090b90607861
ahymery <ahymery22@gmail.com>
2024-02-28 20:52:46 +0100

    Connexion ajouté manque sessions

--- form_connexion.php
+++ form_connexion.php
@@ -15,23 +15,24 @@
         <br><input type="submit" name="btn-connexion" class="btn btn-primary btn-sm" value=" &#128275 Connexion"></input>
     </form>
     <?php
+
         if(isset($_REQUEST['mel'])){
             require_once('connexion.php');
             $mel = $_REQUEST['mel'];
             $motdepasse = $_REQUEST['motdepasse'];
             
-            $stmt = $connexion->prepare("SELECT * FROM client WHERE MEL=:mel AND MOTDEPASSE=:motdepasse");
+            $stmt = $connexion->prepare("SELECT * FROM client WHERE mel=:mel AND motdepasse=:motdepasse");
             $stmt->bindValue(':mel', $mel, PDO::PARAM_STR);
             $stmt->bindValue(':motdepasse', $motdepasse, PDO::PARAM_STR);
             $stmt->setFetchMode(PDO::FETCH_OBJ);
             $stmt->execute();
-            
-            if($_REQUEST['mel'] <> $stmt || $_REQUEST['motdepasse'] <> $stmt){
-                echo 'Mel ou mot de passe incorrect';
-            } elseif($_REQUEST['mel'] == $stmt) {
-                echo 'ca marche';   
-            }
+            $enregistrement = $stmt->fetch();
+        if(!$enregistrement){
+            echo 'Mel ou mot de passe incorrect';
+        } elseif($enregistrement) {
+                    echo 'Bienvenue ' .$mel.'';   
         }
-    ?>
+    }
+?>
 </body>
 </html>
