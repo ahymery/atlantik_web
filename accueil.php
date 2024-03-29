7342cf2bc40f0a08886e2d49b5b20abf0a64d405
Gold <148552312+ahymery@users.noreply.github.com>
2024-03-29 17:12:35 +0100

    Première mise à jour (début 29/03/2024)

---
+++ accueil.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atlantik | ACCUEIL</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+        <div class="container-fluid">
+            <!-- Liens -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link active"><img src="atlantik_icone.ico" style="width:50px;"></a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" style="margin: 10px;">Afficher les liaisons par secteur</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" style="margin: 10px;">Afficher les tarifs pour une liaison</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" style="margin: 10px;">Visualiser les horaires de traversées</a>
+                </li>
+            </ul>
+
+            
+        </div>
+    </nav>
+</body>
+</html>
